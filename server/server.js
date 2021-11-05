c70d91df96f2a8e419a7bfb89494d571b09f65b5
= <nikolas.syverod@gmail.com>
2021-11-06 00:12:06 +0300

    Add /url/short route and functionality

--- server/server.js
+++ server/server.js
@@ -4,17 +4,34 @@ const app = express();
 const port = 5000 || process.env.PORT;
 const cors = require("cors");
 const db = require("./config/database");
-
+const Url = require("./models/url");
+const { nanoid } = require("nanoid");
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/url/shorten", (req, res) => {
-  const url = req.query.url;
+app.post("/url/shorten", async (req, res) => {
+  try {
+    const url = req.query.url;
+    const urlRegex =
+      /^(?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?((?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2}))|((\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)(\.(\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)){3}))(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$ |\/.,*:;=]|%[a-f\d]{2})*)?$/i;
+    if (!url || !urlRegex.test(url)) {
+      res.status(400).json({ error: "Invalid url!" });
+    }
+    const [urlModel, created] = await Url.findOrCreate({
+      where: { url: url },
+      defaults: { url: url, id: nanoid(8) },
+    });
+    res.status(200).json({ id: urlModel.id });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
 db.authenticate()
-  .then(() => console.log("Database connected!"))
+  .then(() => Url.sync())
   .catch((err) => console.log("Error: " + err));
